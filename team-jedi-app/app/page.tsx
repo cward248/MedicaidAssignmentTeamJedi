cf4992d40753e9abcbb846f524244bd78736c039
manmanx64 <dhuling3@my.stlcc.edu>
2026-02-23 13:27:46 -0600

    Setup for app routing finished

--- team-jedi-app/app/page.tsx
+++ team-jedi-app/app/page.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import RequirmentsPage from "@/components/RequirmentsPage";
+import RequirementsPage from "@/components/RequirementsPage";
 
 export default function Home() {
   return (
     <> 
       <Header />
-      <RequirmentsPage />
+      <RequirementsPage />
       <Footer />
     </>
   );
---
+++ team-jedi-app/app/requirements/page.tsx
@@ -0,0 +1,10 @@
+import {RequirementsPage} from "@/components/RequirementsPage";
+
+export default function Page() {
+
+    return (
+        <main>
+            <RequirementsPage />
+        </main>
+    );
+}
